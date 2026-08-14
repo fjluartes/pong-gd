39acb7a3d59270e24874d7cb1ac1b279fa280780
Fred Luartes <fjluartes@gmail.com>
2026-08-14 11:30:48 +0800

    adds walls

---
+++ ball.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+const speed = 500.0
+var direction = Vector2.ZERO
+var screen_size: Vector2
+
+func _ready() -> void:
+	direction = Vector2(-1, 0.6).normalized() # move toward top-left
+	screen_size = get_viewport_rect().size
+
+func _physics_process(delta: float) -> void:
+	var motion = direction * speed * delta
+	var collision = move_and_collide(motion)
+	
+	if collision:
+		direction = direction.bounce(collision.get_normal())
